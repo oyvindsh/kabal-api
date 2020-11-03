91214656bddf04378485b9b82443d400301ceb7a
Hågen Hasle <haagenhasle@gmail.com>
2020-11-03 09:49:42 +0100

    Endret fra ByProfileStrategy til ByClusterStrategy, som rimer bedre med det vi egentlig er ute etter, og som støtter flere verdier i oppsettet. Byttet endepunkt for unleash til det plattform har bestemt skal brukes fremover

---
+++ src/main/kotlin/no/nav/klage/oppgave/service/unleash/ByClusterStrategy.kt
@@ -0,0 +1,25 @@
+package no.nav.klage.oppgave.service.unleash
+
+import no.finn.unleash.strategy.Strategy
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+
+@Component
+class ByClusterStrategy(@Value("\${nais.cluster.name}") val currentCluster: String) : Strategy {
+
+    companion object {
+        const val PARAM = "cluster"
+    }
+
+    override fun getName(): String = "byCluster"
+
+    override fun isEnabled(parameters: Map<String, String>?): Boolean =
+        getEnabledClusters(parameters)?.any { isCurrentClusterEnabled(it) } ?: false
+
+    private fun getEnabledClusters(parameters: Map<String, String>?) =
+        parameters?.get(PARAM)?.split(',')
+
+    private fun isCurrentClusterEnabled(cluster: String): Boolean =
+        currentCluster == cluster
+}
