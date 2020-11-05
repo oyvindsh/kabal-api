054449fcc3fdb96e688036e232731af97a7730ce
Andreas Jonsson <andreas.jonsson@nav.no>
2020-11-05 14:23:00 +0100

    Seems to look better without. Better doc.

--- src/main/kotlin/no/nav/klage/oppgave/config/OpenApiConfig.kt
+++ src/main/kotlin/no/nav/klage/oppgave/config/OpenApiConfig.kt
@@ -8,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.Tag
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import java.time.LocalDate
 
 @Configuration
 class OpenApiConfig {
@@ -20,7 +19,6 @@ class OpenApiConfig {
             .apis(RequestHandlerSelectors.basePackage(OppgaveController::class.java.packageName))
             .build()
             .pathMapping("/")
-            .directModelSubstitute(LocalDate::class.java, String::class.java)
             .genericModelSubstitutes(ResponseEntity::class.java)
             .tags(Tag("klage-oppgave-api", "oppgave-api for saksbehandlere ved klageinstansen"))
     }
