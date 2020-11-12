86b27a4b4f79dc3704c97481ac61d2478d75c1e7
Hågen Hasle <haagenhasle@gmail.com>
2020-11-12 14:17:48 +0100

    Lagt til håndtering av 404 mot Axsys, for ansatte som ikke finnes returnerer vi ingen tilganger i stedet for å feile

--- src/main/kotlin/no/nav/klage/oppgave/clients/AxsysClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/AxsysClient.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Component
@@ -26,18 +27,23 @@ class AxsysClient(private val axsysWebClient: WebClient, private val tracer: Tra
     fun getTilgangerForSaksbehandler(navIdent: String): Tilganger {
         logger.debug("Fetching tilganger for saksbehandler with Nav-Ident {}", navIdent)
 
-        return axsysWebClient.get()
-            .uri { uriBuilder ->
-                uriBuilder
-                    .path("/tilgang/{navIdent}")
-                    .queryParam("inkluderAlleEnheter", "true")
-                    .build(navIdent)
-            }.header("Nav-Call-Id", tracer.currentSpan().context().traceIdString())
-            .header("Nav-Consumer-Id", applicationName)
+        return try {
+            axsysWebClient.get()
+                .uri { uriBuilder ->
+                    uriBuilder
+                        .path("/tilgang/{navIdent}")
+                        .queryParam("inkluderAlleEnheter", "true")
+                        .build(navIdent)
+                }.header("Nav-Call-Id", tracer.currentSpan().context().traceIdString())
+                .header("Nav-Consumer-Id", applicationName)
 
-            .retrieve()
-            .bodyToMono<Tilganger>()
-            .block() ?: throw RuntimeException("Tilganger could not be fetched")
+                .retrieve()
+                .bodyToMono<Tilganger>()
+                .block() ?: throw RuntimeException("Tilganger could not be fetched")
+        } catch (notFound: WebClientResponseException.NotFound) {
+            logger.warn("Got a 404 fetching tilganger for saksbehandler {}, returning empty object", navIdent)
+            Tilganger(emptyArray())
+        }
     }
 
     @Retryable
