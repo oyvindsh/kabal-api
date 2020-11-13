0ea4c859b3dffbfb12fcc5ffe05c76af170cc32b
Hågen Hasle <haagenhasle@gmail.com>
2020-11-13 15:07:36 +0100

    Gått over til onbehalfof mot klage-fss-proxy

--- src/main/kotlin/no/nav/klage/oppgave/clients/KlageProxyClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/KlageProxyClient.kt
@@ -1,7 +1,7 @@
 package no.nav.klage.oppgave.clients
 
-import no.nav.klage.oppgave.config.SecurityConfiguration
-import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.http.HttpHeaders
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
@@ -11,7 +11,8 @@ import org.springframework.web.reactive.function.client.bodyToMono
 @Component
 class KlageProxyClient(
     private val klageProxyWebClient: WebClient,
-    private val tokenValidationContextHolder: TokenValidationContextHolder
+    private val clientConfigurationProperties: ClientConfigurationProperties,
+    private val oAuth2AccessTokenService: OAuth2AccessTokenService
 ) {
 
     @Retryable
@@ -19,14 +20,16 @@ class KlageProxyClient(
     fun getRoller(ident: String): List<String> {
         return klageProxyWebClient.get()
             .uri("/roller/{ident}", ident)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer ${getSaksbehandlersToken()}")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ${getSaksbehandlersTokenWithProxyScope()}")
             .retrieve()
             .bodyToMono<List<String>>()
             .block()
             ?: throw RuntimeException("Unable to get roller for $ident")
     }
 
-    private fun getSaksbehandlersToken() =
-        tokenValidationContextHolder.tokenValidationContext.getJwtToken(SecurityConfiguration.ISSUER_AAD).tokenAsString
-
+    private fun getSaksbehandlersTokenWithProxyScope(): String {
+        val clientProperties = clientConfigurationProperties.registration["proxy-onbehalfof"]
+        val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+        return response.accessToken
+    }
 }
