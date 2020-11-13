4028a5e65167764f395d8a21012dbb52ceca4c7d
Hågen Hasle <haagenhasle@gmail.com>
2020-11-13 23:18:09 +0100

    Unleash i TokenService ga en sirkulær avhengighet, så det måtte jeg droppe

--- src/main/kotlin/no/nav/klage/oppgave/service/TokenService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/TokenService.kt
@@ -1,6 +1,5 @@
 package no.nav.klage.oppgave.service
 
-import no.finn.unleash.Unleash
 import no.nav.klage.oppgave.clients.StsClient
 import no.nav.klage.oppgave.config.SecurityConfiguration
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
@@ -13,8 +12,7 @@ class TokenService(
     private val clientConfigurationProperties: ClientConfigurationProperties,
     private val oAuth2AccessTokenService: OAuth2AccessTokenService,
     private val tokenValidationContextHolder: TokenValidationContextHolder,
-    private val stsClient: StsClient,
-    private val unleash: Unleash
+    private val stsClient: StsClient
 ) {
 
     fun getSaksbehandlerAccessTokenWithGraphScope(): String {
@@ -24,13 +22,10 @@ class TokenService(
     }
 
     fun getSaksbehandlerAccessTokenWithOppgaveScope(): String {
-        return if (unleash.isEnabled("OppgaveMedBrukerkontekst")) {
-            val clientProperties = clientConfigurationProperties.registration["oppgave-onbehalfof"]
-            val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-            response.accessToken
-        } else {
-            getStsSystembrukerToken()
-        }
+        //    val clientProperties = clientConfigurationProperties.registration["oppgave-onbehalfof"]
+        //    val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+        //    return response.accessToken
+        getStsSystembrukerToken()
     }
 
     fun getAppAccessTokenWithGraphScope(): String {
