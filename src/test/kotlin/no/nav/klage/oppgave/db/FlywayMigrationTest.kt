713629cf914863e6e88e5ddf3e375af12e8d5777
Hågen Hasle <haagenhasle@gmail.com>
2020-12-03 21:32:42 +0100

    Disabled flyway test også..

--- src/test/kotlin/no/nav/klage/oppgave/db/FlywayMigrationTest.kt
+++ src/test/kotlin/no/nav/klage/oppgave/db/FlywayMigrationTest.kt
@@ -1,6 +1,7 @@
 package no.nav.klage.oppgave.db
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -11,6 +12,7 @@ import java.sql.ResultSet
 
 @ActiveProfiles("local")
 @DataJpaTest
+@Disabled
 class FlywayMigrationTest {
 
     @Autowired
