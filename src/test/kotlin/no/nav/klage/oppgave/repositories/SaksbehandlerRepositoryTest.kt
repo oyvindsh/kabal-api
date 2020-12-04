0c2050f4fc28bd8c129eb14420421b25a161e847
Richard Borge <richard.borge@nav.no>
2020-12-04 15:40:03 +0100

    Changing assertion

--- src/test/kotlin/no/nav/klage/oppgave/repositories/SaksbehandlerRepositoryTest.kt
+++ src/test/kotlin/no/nav/klage/oppgave/repositories/SaksbehandlerRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.klage.oppgave.clients.KlageProxyClient
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -26,13 +27,13 @@ internal class SaksbehandlerRepositoryTest {
     @Test
     fun `riktig rolle returnerer true`() {
         val erSaksbehandler = saksbehandlerRepository.erSaksbehandler("Z123456")
-        assert(erSaksbehandler)
+        Assertions.assertTrue(erSaksbehandler)
     }
 
     @Test
     fun `feil rolle returnerer false`() {
         val erSaksbehandler = saksbehandlerRepository.erSaksbehandler("Z654321")
-        assert(!erSaksbehandler)
+        Assertions.assertFalse(erSaksbehandler)
     }
 
 }
