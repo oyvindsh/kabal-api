1de8672e919f92f339b90bc1810b0a099f459d37
Andreas Jonsson <andreas.jonsson@nav.no>
2020-12-04 17:13:12 +0100

    Get enheter

---
+++ src/main/kotlin/no/nav/klage/oppgave/api/SaksbehandlerController.kt
@@ -0,0 +1,47 @@
+package no.nav.klage.oppgave.api
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import no.nav.klage.oppgave.config.SecurityConfiguration
+import no.nav.klage.oppgave.domain.Tilganger
+import no.nav.klage.oppgave.domain.view.Enhet
+import no.nav.klage.oppgave.service.SaksbehandlerService
+import no.nav.klage.oppgave.util.getLogger
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Api(tags = ["klage-oppgave-api"])
+@ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
+class SaksbehandlerController(private val saksbehandlerService: SaksbehandlerService) {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
+    @ApiOperation(
+        value = "Hent enheter for en ansatt",
+        notes = "Henter alle enheter som saksbehandler er knyttet til."
+    )
+    @GetMapping("/ansatte/{navIdent}/enheter", produces = ["application/json"])
+    fun getEnheter(
+        @ApiParam(value = "NavIdent til en ansatt")
+        @PathVariable navIdent: String
+    ): List<Enhet> {
+        logger.debug("getEnheter is requested by $navIdent")
+        return saksbehandlerService.getTilgangerForSaksbehandler().toEnheter()
+    }
+
+    private fun Tilganger.toEnheter() =
+        this.enheter.map {
+            Enhet(
+                id = it.enhetId,
+                navn = it.navn
+            )
+        }
+}
+
---
+++ src/main/kotlin/no/nav/klage/oppgave/domain/view/Enhet.kt
@@ -0,0 +1,6 @@
+package no.nav.klage.oppgave.domain.view
+
+data class Enhet(
+    val id: String,
+    val navn: String
+)
