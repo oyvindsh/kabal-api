bc07efbff1a8cf53abb723c61ac62b43095536f9
Hågen Hasle <haagenhasle@gmail.com>
2020-12-14 13:19:50 +0100

    Lagt til testene som var i jdbc-branchen, og en OppgaveKopiService som overbygg som må til for å klage å håndtere idempotency riktig

--- src/main/kotlin/no/nav/klage/oppgave/repositories/OppgaveKopiVersjonRepository.kt
+++ src/main/kotlin/no/nav/klage/oppgave/repositories/OppgaveKopiVersjonRepository.kt
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OppgaveKopiVersjonRepository : JpaRepository<OppgaveKopiVersjon, OppgaveKopiVersjonId>
+interface OppgaveKopiVersjonRepository : JpaRepository<OppgaveKopiVersjon, OppgaveKopiVersjonId> {
+
+    fun findFirstByIdOrderByVersjonDesc(oppgaveKopiId: Long): OppgaveKopiVersjon
+}
