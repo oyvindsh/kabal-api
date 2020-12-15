e4d9e4feb5075b7e7ed489776b68a58a4d4f9907
Andreas Jonsson <andreas.jonsson@nav.no>
2020-12-15 15:23:44 +0100

    Also return IT-avd.-enhet for debugging purposes.

--- src/main/kotlin/no/nav/klage/oppgave/clients/AxsysClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/AxsysClient.kt
@@ -22,6 +22,7 @@ class AxsysClient(private val axsysWebClient: WebClient, private val tracer: Tra
         private val logger = getLogger(javaClass.enclosingClass)
 
         const val KLAGEENHET_PREFIX = "42"
+        const val IT_ENHET = "2990"
     }
 
     @Value("\${spring.application.name}")
@@ -46,9 +47,12 @@ class AxsysClient(private val axsysWebClient: WebClient, private val tracer: Tra
                 .bodyToMono<Tilganger>()
                 .block() ?: throw RuntimeException("Tilganger could not be fetched")
 
-//            Tilganger(
-//                enheter = tilganger.enheter.filter { enhet -> enhet.enhetId.startsWith(KLAGEENHET_PREFIX) }
-//            )
+            Tilganger(
+                enheter = tilganger.enheter.filter { enhet ->
+                    enhet.enhetId.startsWith(KLAGEENHET_PREFIX) ||
+                    enhet.enhetId == IT_ENHET
+                }
+            )
             tilganger
         } catch (notFound: WebClientResponseException.NotFound) {
             logger.warn("Got a 404 fetching tilganger for saksbehandler {}, returning empty object", navIdent)
