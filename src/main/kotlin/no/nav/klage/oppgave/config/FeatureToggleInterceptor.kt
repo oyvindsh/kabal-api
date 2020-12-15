1191a27d932fe8e7ee18dd8b75b4866482990e08
Andreas Jonsson <andreas.jonsson@nav.no>
2020-12-15 15:32:40 +0100

    Cleaning up after refactor

--- src/main/kotlin/no/nav/klage/oppgave/config/FeatureToggleInterceptor.kt
+++ src/main/kotlin/no/nav/klage/oppgave/config/FeatureToggleInterceptor.kt
@@ -3,8 +3,6 @@ package no.nav.klage.oppgave.config
 import no.finn.unleash.Unleash
 import no.nav.klage.oppgave.config.FeatureToggleConfig.Companion.KLAGE_GENERELL_TILGANG
 import no.nav.klage.oppgave.exceptions.FeatureNotEnabledException
-import no.nav.klage.oppgave.repositories.InnloggetSaksbehandlerRepository
-import no.nav.klage.oppgave.util.getLogger
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
@@ -14,15 +12,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class FeatureToggleInterceptor(
-    private val unleash: Unleash,
-    private val innloggetSaksbehandlerRepository: InnloggetSaksbehandlerRepository
-) : HandlerInterceptorAdapter() {
-
-    companion object {
-        @Suppress("JAVA_CLASS_ON_COMPANION")
-        private val logger = getLogger(javaClass.enclosingClass)
-    }
+class FeatureToggleInterceptor(private val unleash: Unleash) : HandlerInterceptorAdapter() {
 
     @Throws(Exception::class)
     override fun preHandle(
@@ -30,16 +20,13 @@ class FeatureToggleInterceptor(
         response: HttpServletResponse,
         handler: Any?
     ): Boolean {
-        val isEnabled = isEnabled(KLAGE_GENERELL_TILGANG)
+        val isEnabled = unleash.isEnabled(KLAGE_GENERELL_TILGANG)
         if (!isEnabled) {
             throw FeatureNotEnabledException("Du er ikke gitt tilgang til klage-oppgave-api")
         }
         return isEnabled
     }
 
-    private fun isEnabled(feature: String): Boolean {
-        return unleash.isEnabled(feature)
-    }
 }
 
 @Configuration
