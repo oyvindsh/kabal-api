4eedb3e30d80368fb784938ce82cd3b6c08e5bc9
Richard Borge <richard.borge@nav.no>
2021-01-11 11:49:57 +0100

    Adding transactional

--- src/main/kotlin/no/nav/klage/oppgave/service/BehandlingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/BehandlingService.kt
@@ -2,12 +2,12 @@ package no.nav.klage.oppgave.service
 
 import no.nav.klage.oppgave.domain.klage.Behandling
 import no.nav.klage.oppgave.repositories.BehandlingRepository
-import no.nav.klage.oppgave.repositories.DokumentRepository
 import no.nav.klage.oppgave.repositories.KlagesakRepository
 import org.springframework.stereotype.Service
-import java.util.*
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class BehandlingService(
     private val klagesakRepository: KlagesakRepository,
     private val behandlingRepository: BehandlingRepository
