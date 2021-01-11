7a77f4a6848d81c5b78aaa5a66bc1ac41f448ef4
Richard Borge <richard.borge@nav.no>
2021-01-11 11:42:47 +0100

    Adding behandlingService

---
+++ src/main/kotlin/no/nav/klage/oppgave/repositories/KlagesakRepository.kt
@@ -0,0 +1,10 @@
+package no.nav.klage.oppgave.repositories
+
+import no.nav.klage.oppgave.domain.klage.Behandling
+import no.nav.klage.oppgave.domain.klage.Klagesak
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface KlagesakRepository : JpaRepository<Klagesak, UUID>
