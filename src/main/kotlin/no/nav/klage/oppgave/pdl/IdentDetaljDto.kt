c74c867600d034ed623e49ef10cfeeac35c975a5
Hågen Hasle <haagenhasle@gmail.com>
2021-01-12 09:16:21 +0100

    Lagt til metadata i IdentDetaljDto

--- src/main/kotlin/no/nav/klage/oppgave/pdl/IdentDetaljDto.kt
+++ src/main/kotlin/no/nav/klage/oppgave/pdl/IdentDetaljDto.kt
@@ -3,7 +3,9 @@ package no.nav.klage.oppgave.pdl
 data class IdentDetaljDto(
     val ident: String,
     val historisk: Boolean,
-    val gruppe: IdentGruppeDto
+    val gruppe: IdentGruppeDto,
+    val folkeregistermetadata: FolkeregistermetadataDto?,
+    val metadata: MetadataDto
 )
 
 enum class IdentGruppeDto {
