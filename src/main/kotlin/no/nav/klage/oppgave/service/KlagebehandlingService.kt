2c5589920c0cf24bdca5b747c2bcd6a30705e749
Richard Borge <richard.borge@nav.no>
2021-01-29 10:01:32 +0100

    Checking for klageinstans on oppgavekopi

--- src/main/kotlin/no/nav/klage/oppgave/service/KlagebehandlingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/KlagebehandlingService.kt
@@ -5,6 +5,7 @@ import no.nav.klage.oppgave.domain.klage.Oppgavereferanse
 import no.nav.klage.oppgave.domain.oppgavekopi.OppgaveKopi
 import no.nav.klage.oppgave.repositories.KlagebehandlingRepository
 import no.nav.klage.oppgave.repositories.SakstypeRepository
+import no.nav.klage.oppgave.util.getLogger
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,37 +20,40 @@ class KlagebehandlingService(
     private val sakstypeRepository: SakstypeRepository
 ) {
 
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+        private val klageinstansPrefix = "42"
+    }
+
     fun fetchKlagesakForOppgaveKopi(oppgaveId: Long): Klagebehandling? =
         klagebehandlingRepository.findByOppgavereferanserOppgaveId(oppgaveId)
 
-    fun connectOppgaveKopiToKlagebehandling(oppgaveKopi: OppgaveKopi): UUID {
+    fun connectOppgaveKopiToKlagebehandling(oppgaveKopi: OppgaveKopi) {
         val klagesak = fetchKlagesakForOppgaveKopi(oppgaveKopi.id)
-        if (klagesak != null) {
-            return klagesak.id
-        }
+        if (klagesak == null && oppgaveKopi.tildeltEnhetsnr.startsWith(klageinstansPrefix)) {
+            requireNotNull(oppgaveKopi.ident)
+            requireNotNull(oppgaveKopi.behandlingstype)
 
-        requireNotNull(oppgaveKopi.ident)
-        requireNotNull(oppgaveKopi.behandlingstype)
-
-        val createdKlagebehandling = klagebehandlingRepository.save(
-            Klagebehandling(
-                foedselsnummer = oppgaveKopi.ident.folkeregisterident
-                    ?: throw RuntimeException("folkeregisterident is missing from oppgave"),
-                tema = oppgaveKopi.tema,
-                frist = oppgaveKopi.fristFerdigstillelse ?: calculateFrist(),
-                sakstype = sakstypeRepository.findByIdOrNull(oppgaveKopi.behandlingstype)
-                    ?: throw RuntimeException("No sakstype found for ${oppgaveKopi.id}"),
-                hjemler = hjemmelService.getHjemmelFromOppgaveKopi(oppgaveKopi),
-                tildeltSaksbehandlerident = oppgaveKopi.tilordnetRessurs,
-                oppgavereferanser = listOf(
-                    Oppgavereferanse(
-                        oppgaveId = oppgaveKopi.id
+            val createdKlagebehandling = klagebehandlingRepository.save(
+                Klagebehandling(
+                    foedselsnummer = oppgaveKopi.ident.folkeregisterident
+                        ?: throw RuntimeException("folkeregisterident is missing from oppgave"),
+                    tema = oppgaveKopi.tema,
+                    frist = oppgaveKopi.fristFerdigstillelse ?: calculateFrist(),
+                    sakstype = sakstypeRepository.findByIdOrNull(oppgaveKopi.behandlingstype)
+                        ?: throw RuntimeException("No sakstype found for ${oppgaveKopi.id}"),
+                    hjemler = hjemmelService.getHjemmelFromOppgaveKopi(oppgaveKopi),
+                    tildeltSaksbehandlerident = oppgaveKopi.tilordnetRessurs,
+                    oppgavereferanser = listOf(
+                        Oppgavereferanse(
+                            oppgaveId = oppgaveKopi.id
+                        )
                     )
                 )
             )
-        )
-
-        return createdKlagebehandling.id
+            logger.debug("Created behandling ${createdKlagebehandling.id} for oppgave ${oppgaveKopi.id}")
+        }
     }
 
     fun getKlagebehandlingByOppgaveId(oppgaveId: Long): Klagebehandling {
