d9e11e3131e5c929930bb3e1de4f046e05a14750
Hågen Hasle <haagenhasle@gmail.com>
2021-02-04 20:03:58 +0100

    Gjort om kodeverket til enums og fjernet den ene Repositoryen som var med

--- src/main/kotlin/no/nav/klage/oppgave/repositories/SakstypeRepository.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.klage.oppgave.repositories
-
-import no.nav.klage.oppgave.domain.kodeverk.Sakstype
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface SakstypeRepository : JpaRepository<Sakstype, String>
