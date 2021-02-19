d4d05c4fc39747ec4178ccbe67fc7af60b4b1bb8
Hågen Hasle <haagenhasle@gmail.com>
2021-02-19 10:32:29 +0100

    Lagt til unprotected på elasticadmin-endepunktet

--- src/main/kotlin/no/nav/klage/oppgave/api/ElasticsearchController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/ElasticsearchController.kt
@@ -2,6 +2,7 @@ package no.nav.klage.oppgave.api
 
 import no.nav.klage.oppgave.repositories.ElasticsearchRepository
 import no.nav.klage.oppgave.util.getLogger
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +14,7 @@ class ElasticsearchController(private val elasticsearchRepository: Elasticsearch
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
+    @Unprotected
     @GetMapping("/internal/elasticadmin/nuke", produces = ["application/json"])
     fun resetElasticIndex(): ElasticAdminResponse {
         //TODO: Make this more fancy.. Need auth, need a service, need to reindex from db.
