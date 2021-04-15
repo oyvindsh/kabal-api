23c7e6c3448e25acedd51d3f48fbf02813ef599b
Hågen Hasle <haagenhasle@gmail.com>
2021-04-15 17:38:01 +0200

    Fikset feil i ES-mapping

--- src/test/kotlin/no/nav/klage/oppgave/service/CreateIndexFromEsKlagebehandlingTest.kt
+++ src/test/kotlin/no/nav/klage/oppgave/service/CreateIndexFromEsKlagebehandlingTest.kt
@@ -30,7 +30,7 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @SpringBootTest(classes = [RetryConfig::class])
 @ImportAutoConfiguration(ElasticsearchRestClientAutoConfiguration::class, ElasticsearchDataAutoConfiguration::class)
 @Disabled("kan brukes for å generere settings og mapping, for så å lagre som fil. Må da endre i ElasticsearchService")
-class CreateIndexFromEsOppgaveTest {
+class CreateIndexFromEsKlagebehandlingTest {
 
     companion object {
         @Container
