3a0815d5a6a80ed72da3a3cf2c73da34532947a7
Hågen Hasle <haagenhasle@gmail.com>
2021-04-22 11:32:25 +0200

    Fikset kodeverkresponse, må ha egen dto for å få jsonifisert alle feltene vi ønsker oss

--- src/main/kotlin/no/nav/klage/oppgave/api/view/KodeverkResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/KodeverkResponse.kt
@@ -3,11 +3,15 @@ package no.nav.klage.oppgave.api.view
 import no.nav.klage.oppgave.domain.kodeverk.*
 
 data class KodeverkResponse(
-    val eoes: List<Kode> = Eoes.values().asList(),
-    val grunn: List<Kode> = Grunn.values().asList(),
-    val hjemmel: List<Kode> = Hjemmel.values().asList(),
-    val raadfoertMedLege: List<Kode> = RaadfoertMedLege.values().asList(),
-    val type: List<Kode> = Type.values().asList(),
-    val tema: List<Kode> = Tema.values().asList(),
-    val utfall: List<Kode> = Utfall.values().asList(),
+    val eoes: List<Kode> = Eoes.values().asList().toDto(),
+    val grunn: List<Kode> = Grunn.values().asList().toDto(),
+    val hjemmel: List<Kode> = Hjemmel.values().asList().toDto(),
+    val raadfoertMedLege: List<Kode> = RaadfoertMedLege.values().asList().toDto(),
+    val type: List<Kode> = Type.values().asList().toDto(),
+    val tema: List<Kode> = Tema.values().asList().toDto(),
+    val utfall: List<Kode> = Utfall.values().asList().toDto(),
 )
+
+data class KodeDto(override val id: Int, override val navn: String, override val beskrivelse: String) : Kode
+
+fun List<Kode>.toDto() = map { KodeDto(it.id, it.navn, it.beskrivelse) }
