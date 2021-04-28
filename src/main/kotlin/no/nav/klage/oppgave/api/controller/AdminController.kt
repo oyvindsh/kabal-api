2c83be1e7ff59c8eea31155cd2763124e65db3bc
Hågen Hasle <haagenhasle@gmail.com>
2021-04-28 20:58:38 +0200

    lagt til post-endepunkt for es rebuild

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/AdminController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/AdminController.kt
@@ -8,7 +8,10 @@ import no.nav.klage.oppgave.service.AdminService
 import no.nav.klage.oppgave.util.getLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -23,6 +26,20 @@ class AdminController(
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
+    }
+
+    @PostMapping("/internal/elasticadmin/rebuild", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
+    fun resetElasticIndexWithPost() {
+        krevAdminTilgang()
+        try {
+            adminService.recreateEsIndex()
+            adminService.syncEsWithDb()
+            adminService.findAndLogOutOfSyncKlagebehandlinger()
+        } catch (e: Exception) {
+            logger.warn("Failed to reset ES index", e)
+            throw e
+        }
     }
 
     @GetMapping("/internal/elasticadmin/nuke", produces = ["application/json"])
