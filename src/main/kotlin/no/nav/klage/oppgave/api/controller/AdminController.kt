2b77b9581c3ff4d27e02e2d5ca8543b2f2d30ddf
Hågen Hasle <haagenhasle@gmail.com>
2021-04-28 17:13:47 +0200

    Lagt til en admin-rolle, som i prod er klage-teamgruppa vår og i dev gosys-saksbehandler-rollen. Bruker denne adminrollen i auth i AdminController

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/AdminController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/AdminController.kt
@@ -1,23 +1,29 @@
 package no.nav.klage.oppgave.api.controller
 
+import no.nav.klage.oppgave.config.SecurityConfiguration
+import no.nav.klage.oppgave.exceptions.MissingTilgangException
+import no.nav.klage.oppgave.repositories.InnloggetSaksbehandlerRepository
 import no.nav.klage.oppgave.service.AdminService
 import no.nav.klage.oppgave.util.getLogger
-import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AdminController(private val adminService: AdminService) {
+@ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
+class AdminController(
+    private val adminService: AdminService,
+    private val innloggetSaksbehandlerRepository: InnloggetSaksbehandlerRepository
+) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
-    @Unprotected
     @GetMapping("/internal/elasticadmin/nuke", produces = ["application/json"])
     fun resetElasticIndex(): ElasticAdminResponse {
-        //TODO: Trenger auth så ikke hvem som helst kan kalle denne
+        krevAdminTilgang()
         try {
             adminService.recreateEsIndex()
             adminService.syncEsWithDb()
@@ -28,6 +34,12 @@ class AdminController(private val adminService: AdminService) {
         }
 
         return ElasticAdminResponse("ok")
+    }
+
+    private fun krevAdminTilgang() {
+        if (!innloggetSaksbehandlerRepository.erAdmin()) {
+            throw MissingTilgangException("Not an admin")
+        }
     }
 }
 
