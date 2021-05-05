3b80f7ac4c7023393f8011c452e00620c01a5ff9
Hågen Hasle <haagenhasle@gmail.com>
2021-05-05 12:58:11 +0200

    Gjort klagebehandlingVersjon obligatorisk i Input-klassene

--- src/main/kotlin/no/nav/klage/oppgave/api/view/Saksbehandlertildeling.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/Saksbehandlertildeling.kt
@@ -3,10 +3,10 @@ package no.nav.klage.oppgave.api.view
 data class Saksbehandlertildeling(
     val navIdent: String,
     val oppgaveversjon: String?,
-    val klagebehandlingVersjon: Long? = null
+    val klagebehandlingVersjon: Long
 )
 
 data class Saksbehandlerfradeling(
     val oppgaveversjon: String?,
-    val klagebehandlingVersjon: Long? = null
+    val klagebehandlingVersjon: Long
 )
--- src/main/kotlin/no/nav/klage/oppgave/api/view/VedtakInput.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/VedtakInput.kt
@@ -2,16 +2,28 @@ package no.nav.klage.oppgave.api.view
 
 import org.springframework.web.multipart.MultipartFile
 
-data class VedtakUtfallInput(val utfall: String, val klagebehandlingVersjon: Long? = null)
+data class VedtakUtfallInput(
+    val utfall: String,
+    val klagebehandlingVersjon: Long
+)
 
-data class VedtakGrunnInput(val grunn: String?, val klagebehandlingVersjon: Long? = null)
+data class VedtakGrunnInput(
+    val grunn: String?,
+    val klagebehandlingVersjon: Long
+)
 
-data class VedtakHjemlerInput(val hjemler: List<String>?, val klagebehandlingVersjon: Long? = null)
+data class VedtakHjemlerInput(
+    val hjemler: List<String>?,
+    val klagebehandlingVersjon: Long
+)
 
 data class VedtakVedleggInput(
     val vedlegg: MultipartFile,
     val journalfoerendeEnhet: String,
-    val klagebehandlingVersjon: Long? = null
+    val klagebehandlingVersjon: Long
 )
 
-data class VedtakFullfoerInput(val journalfoerendeEnhet: String, val klagebehandlingVersjon: Long? = null)
+data class VedtakFullfoerInput(
+    val journalfoerendeEnhet: String,
+    val klagebehandlingVersjon: Long
+)
