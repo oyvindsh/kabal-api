d88809d494674c82addbc6a69d469ba30950325d
Andreas Jonsson <andreas.jonsson@nav.no>
2021-05-20 10:37:19 +0200

    wip endringstid

--- src/main/kotlin/no/nav/klage/oppgave/eventlisteners/StatistikkTilDVHEventListener.kt
+++ src/main/kotlin/no/nav/klage/oppgave/eventlisteners/StatistikkTilDVHEventListener.kt
@@ -11,6 +11,7 @@ import no.nav.klage.oppgave.service.StatistikkTilDVHKafkaProducer
 import no.nav.klage.oppgave.util.getLogger
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Service
@@ -70,6 +71,8 @@ class StatistikkTilDVHEventListener(
         val vedtak = klagebehandling.vedtak.firstOrNull()
         val now = LocalDateTime.now()
 
+        val funksjoneltEndringstidspunkt = getFunksjoneltEndringstidspunkt(klagebehandling, klagebehandlingState)
+
         return KlageStatistikkTilDVH(
             behandlingId = mottak.dvhReferanse,
             behandlingIdKabal = klagebehandling.id.toString(),
@@ -77,7 +80,7 @@ class StatistikkTilDVHEventListener(
             behandlingStatus = klagebehandlingState.name,
             behandlingType = klagebehandling.type.navn,
             beslutter = klagebehandling.medunderskriverident,
-            endringstid = now,
+            endringstid = funksjoneltEndringstidspunkt.atTime(0, 0),
             hjemmel = klagebehandling.hjemler.map { it.toSearchableString() },
             klager = getPart(klagebehandling.klager.partId.type, klagebehandling.klager.partId.value),
             omgjoeringsgrunn = vedtak?.grunn?.navn,
@@ -92,6 +95,21 @@ class StatistikkTilDVHEventListener(
             vedtaksdato = vedtak?.finalized?.toLocalDate(),
             ytelseType = "TODO"
         )
+    }
+
+    private fun getFunksjoneltEndringstidspunkt(
+        klagebehandling: Klagebehandling,
+        klagebehandlingState: KlagebehandlingState
+    ): LocalDate {
+        return when (klagebehandlingState) {
+            MOTTATT -> klagebehandling.mottattKlageinstans
+            TILDELT_SAKSBEHANDLER -> klagebehandling.tildelt ?: throw RuntimeException("tildelt mangler")
+            AVSLUTTET -> klagebehandling.avsluttet ?: throw RuntimeException("avsluttet mangler")
+            UKJENT -> {
+                logger.warn("Unknown funksjoneltEndringstidspunkt")
+                LocalDate.now()
+            }
+        }
     }
 
     private fun getPart(type: PartIdType, value: String) =
