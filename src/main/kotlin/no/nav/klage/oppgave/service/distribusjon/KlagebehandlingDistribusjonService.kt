7604993d21af2fa243e03d7f6a2ab172ee0f7bb1
Hågen Hasle <haagenhasle@gmail.com>
2021-05-25 21:05:32 +0200

    Kode for å distribuere vedtak som er markert som klare for distribusjon av saksbehandler

---
+++ src/main/kotlin/no/nav/klage/oppgave/service/distribusjon/KlagebehandlingDistribusjonService.kt
@@ -0,0 +1,68 @@
+package no.nav.klage.oppgave.service.distribusjon
+
+import no.nav.klage.oppgave.domain.klage.BrevMottaker
+import no.nav.klage.oppgave.domain.klage.Klagebehandling
+import no.nav.klage.oppgave.domain.klage.Vedtak
+import no.nav.klage.oppgave.service.KlagebehandlingService
+import no.nav.klage.oppgave.util.getLogger
+import no.nav.klage.oppgave.util.getSecureLogger
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@Transactional
+class KlagebehandlingDistribusjonService(
+    private val klagebehandlingService: KlagebehandlingService,
+    private val vedtakDistribusjonService: VedtakDistribusjonService,
+    private val klagebehandlingAvslutningService: KlagebehandlingAvslutningService
+) {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun distribuerKlagebehandling(klagebehandlingId: UUID) {
+        try {
+            val klagebehandling = klagebehandlingService.getKlagebehandlingForUpdate(klagebehandlingId, null)
+            klagebehandling.vedtak
+                .filter { null == it.ferdigDistribuert }
+                .forEach { vedtak ->
+                    if (vedtak.brevmottakere.isEmpty()) {
+                        vedtakDistribusjonService.lagBrevmottakere(klagebehandling, vedtak)
+                    }
+                    vedtak.brevmottakere.forEach { brevMottaker ->
+                        if (brevMottaker.journalpostId != vedtak.journalpostId) {
+                            vedtakDistribusjonService.lagKopiAvJournalpostForMottaker(vedtak, brevMottaker)
+                        }
+                        distribuerVedtakTilMottaker(klagebehandling, vedtak, brevMottaker)
+                    }
+                    vedtakDistribusjonService.markerVedtakSomFerdigDistribuert(klagebehandling, vedtak)
+                }
+            klagebehandlingAvslutningService.avsluttKlagebehandling(klagebehandling)
+        } catch (e: Exception) {
+            logger.error("Feilet under distribuering av klagebehandling $klagebehandlingId", e)
+        }
+    }
+
+
+    private fun distribuerVedtakTilMottaker(
+        klagebehandling: Klagebehandling,
+        vedtak: Vedtak,
+        brevMottaker: BrevMottaker,
+    ) {
+        if (brevMottaker.dokdistReferanse != null) {
+            vedtakDistribusjonService.distribuerJournalpostTilMottaker(
+                klagebehandling,
+                vedtak,
+                brevMottaker,
+                VedtakDistribusjonService.SYSTEMBRUKER,
+                klagebehandling.tildeltEnhet!!
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/no/nav/klage/oppgave/service/distribusjon/KlagebehandlingSchedulerService.kt
@@ -0,0 +1,26 @@
+package no.nav.klage.oppgave.service.distribusjon
+
+import no.nav.klage.oppgave.service.KlagebehandlingService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@Transactional
+class KlagebehandlingSchedulerService(
+    private val klagebehandlingService: KlagebehandlingService,
+    private val klagebehandlingDistribusjonService: KlagebehandlingDistribusjonService
+) {
+
+    @Scheduled(cron = "0 0 3 * * *", zone = "Europe/Paris")
+    @Transactional
+    fun distribuerVedtak() {
+
+        val klagebehandlinger: List<UUID> = klagebehandlingService.findKlagebehandlingForDistribusjon()
+
+        klagebehandlinger.forEach { klagebehandling ->
+            klagebehandlingDistribusjonService.distribuerKlagebehandling(klagebehandling)
+        }
+    }
+}
