fdd0136628db28884fd3938e26ae898461f58055
Hågen Hasle <haagenhasle@gmail.com>
2021-05-26 11:24:04 +0200

    Lagt til støtte for shedlock så scheduled tasks bare skal kjøre på en pod

--- src/main/kotlin/no/nav/klage/oppgave/config/SchedulingConfiguration.kt
+++ src/main/kotlin/no/nav/klage/oppgave/config/SchedulingConfiguration.kt
@@ -1,11 +1,30 @@
 package no.nav.klage.oppgave.config
 
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
+import javax.sql.DataSource
+
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 @EnableAsync
 class SchedulingConfiguration {
+
+    @Bean
+    fun lockProvider(dataSource: DataSource): LockProvider {
+        return JdbcTemplateLockProvider(
+            JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(JdbcTemplate(dataSource))
+                .withTableName("klage.shedlock")
+                .usingDbTime()
+                .build()
+        )
+    }
 }
