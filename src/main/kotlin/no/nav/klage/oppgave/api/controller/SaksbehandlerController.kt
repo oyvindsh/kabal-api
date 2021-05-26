8ecab99d8870acfbdf9a4892904b8f8f10355200
Hågen Hasle <haagenhasle@gmail.com>
2021-05-26 14:37:51 +0200

    Unprotected nå på klassenivå

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/SaksbehandlerController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/SaksbehandlerController.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+@Unprotected
 @RestController
 @Api(tags = ["kabal-api"])
 class SaksbehandlerController(private val saksbehandlerService: SaksbehandlerService) {
