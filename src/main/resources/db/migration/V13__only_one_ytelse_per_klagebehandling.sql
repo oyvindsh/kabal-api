23c8eb674ce801232d0b8a850cd0ac87e45ef8a4
Andreas Jonsson <andreas.jonsson@nav.no>
2021-09-14 13:20:19 +0200

    Wait to drop column.

--- src/main/resources/db/migration/V13__only_one_ytelse_per_klagebehandling.sql
+++ src/main/resources/db/migration/V13__only_one_ytelse_per_klagebehandling.sql
@@ -5,4 +5,8 @@ UPDATE klage.klagebehandling k
 SET vedtak_id = (SELECT v.id FROM klage.vedtak v WHERE v.klagebehandling_id = k.id);
 
 ALTER TABLE klage.vedtak
-    DROP COLUMN klagebehandling_id;
+    ALTER COLUMN klagebehandling_id drop not null;
+
+-- TODO When verified that everything works
+-- ALTER TABLE klage.vedtak
+--     DROP COLUMN klagebehandling_id;
