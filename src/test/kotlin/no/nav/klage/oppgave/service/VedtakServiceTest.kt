e1887208c8dd4810b71711c381def9faa207f6e6
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-09-23 16:46:07 +0200

    Ryddet i testfil.

--- src/test/kotlin/no/nav/klage/oppgave/service/VedtakServiceTest.kt
+++ src/test/kotlin/no/nav/klage/oppgave/service/VedtakServiceTest.kt
@@ -2,31 +2,20 @@ package no.nav.klage.oppgave.service
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
-import io.mockk.mockk
 import no.nav.klage.oppgave.api.view.VedtakFullfoerInput
-import no.nav.klage.oppgave.db.TestPostgresqlContainer
 import no.nav.klage.oppgave.domain.klage.*
 import no.nav.klage.oppgave.domain.kodeverk.*
 import no.nav.klage.oppgave.exceptions.MissingTilgangException
 import no.nav.klage.oppgave.exceptions.VedtakFinalizedException
 import no.nav.klage.oppgave.exceptions.VedtakNotFoundException
 import no.nav.klage.oppgave.gateway.JournalpostGateway
-import no.nav.klage.oppgave.repositories.KlagebehandlingRepository
-import no.nav.klage.oppgave.repositories.MottakRepository
 import no.nav.klage.oppgave.util.AttachmentValidator
-import no.nav.klage.oppgave.util.TokenUtil
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -37,7 +26,8 @@ import java.util.*
 @SpringBootTest(classes = [KlagebehandlingService::class])
 class VedtakServiceTest {
 
-    private val tilgangService: TilgangService = mockk()
+    @MockkBean
+    lateinit var tilgangService: TilgangService
 
     @MockkBean(relaxed = true)
     lateinit var applicationEventPublisher: ApplicationEventPublisher
@@ -54,7 +44,6 @@ class VedtakServiceTest {
     @MockkBean
     lateinit var journalpostGateway: JournalpostGateway
 
-//    @Autowired
     lateinit var vedtakService: VedtakService
 
     @BeforeEach
@@ -76,8 +65,6 @@ class VedtakServiceTest {
     private val KLAGEBEHANDLING_ID = UUID.randomUUID()
 
 
-
-
     @Test
     fun `Forsøk på avslutting av vedtak fra andre enn medunderskriver skal ikke lykkes`() {
         every { klagebehandlingService.getKlagebehandlingForUpdate(any(), any(), any()) } returns getKlageBehandling()
@@ -95,7 +82,13 @@ class VedtakServiceTest {
 
     @Test
     fun `Forsøk på avslutting av vedtak som allerede er ferdigstilt i Joark skal ikke lykkes`() {
-        every { klagebehandlingService.getKlagebehandlingForUpdate(any(), any(), any()) } returns getFerdigstiltKlagebehandling()
+        every {
+            klagebehandlingService.getKlagebehandlingForUpdate(
+                any(),
+                any(),
+                any()
+            )
+        } returns getFerdigstiltKlagebehandling()
         assertThrows<VedtakFinalizedException> {
             vedtakService.ferdigstillVedtak(
                 KLAGEBEHANDLING_ID,
@@ -140,16 +133,27 @@ class VedtakServiceTest {
 
     @Test
     fun `Forsøk på avslutting av vedtak som er riktig utfylt skal lykkes`() {
-        every { klagebehandlingService.getKlagebehandlingForUpdate(any(), any(), any()) } returns getKlagebehandlingMedUtfall()
-        every { klagebehandlingService.markerKlagebehandlingSomAvsluttetAvSaksbehandler(any(), any()) } returns getKlagebehandlingMedUtfall()
-        val result = vedtakService.ferdigstillVedtak(
-                KLAGEBEHANDLING_ID,
-                VedtakFullfoerInput(
-                    JOURNALFOERENDE_ENHET,
-                    1L
-                ),
-                MEDUNDERSKRIVER_IDENT
+        every {
+            klagebehandlingService.getKlagebehandlingForUpdate(
+                any(),
+                any(),
+                any()
             )
+        } returns getKlagebehandlingMedUtfall()
+        every {
+            klagebehandlingService.markerKlagebehandlingSomAvsluttetAvSaksbehandler(
+                any(),
+                any()
+            )
+        } returns getKlagebehandlingMedUtfall()
+        val result = vedtakService.ferdigstillVedtak(
+            KLAGEBEHANDLING_ID,
+            VedtakFullfoerInput(
+                JOURNALFOERENDE_ENHET,
+                1L
+            ),
+            MEDUNDERSKRIVER_IDENT
+        )
         assert(result.getVedtakOrException().avsluttetAvSaksbehandler != null)
     }
 
