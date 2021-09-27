4a2601a91d2373fad90593ef4634e8e4cafd65b2
Andreas Jonsson <andreas.jonsson@nav.no>
2021-09-27 09:54:19 +0200

    Make it possible to return vedtak manually i dev.

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/DevOnlyAdminController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/DevOnlyAdminController.kt
@@ -1,17 +1,14 @@
 package no.nav.klage.oppgave.api.controller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.klage.oppgave.domain.kafka.KlagevedtakFattet
-import no.nav.klage.oppgave.domain.kodeverk.Utfall
 import no.nav.klage.oppgave.service.AdminService
 import no.nav.klage.oppgave.service.VedtakKafkaProducer
 import no.nav.klage.oppgave.util.getLogger
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Profile("dev-gcp")
 @RestController
@@ -42,15 +39,19 @@ class DevOnlyAdminController(
     @Unprotected
     @PostMapping("/internal/produce_kafka_message")
     @ResponseStatus(HttpStatus.CREATED)
-    fun putResultOnKafkaTopicForTestingPurposes() {
+    fun putResultOnKafkaTopicForTestingPurposes(
+        @RequestBody json: String
+    ) {
 
-        val klagevedtakFattet = KlagevedtakFattet(
-            kildeReferanse = "deres_ref",
-            kilde = "K9",
-            utfall = Utfall.MEDHOLD,
-            vedtaksbrevReferanse = "journalpost_id_her",
-            kabalReferanse = "unik_ref_til_kabal"
-        )
+        val klagevedtakFattet = jacksonObjectMapper().readValue(json, KlagevedtakFattet::class.java)
+
+//        KlagevedtakFattet(
+//            kildeReferanse = "deres_ref",
+//            kilde = "K9",
+//            utfall = Utfall.MEDHOLD,
+//            vedtaksbrevReferanse = "journalpost_id_her",
+//            kabalReferanse = "unik_ref_til_kabal"
+//        )
 
         vedtakKafkaProducer.sendVedtak(klagevedtakFattet)
     }
