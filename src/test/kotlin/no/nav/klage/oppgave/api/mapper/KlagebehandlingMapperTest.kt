063465aaec738b5cd7576dd65287712adbb8e8b1
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-13 17:17:48 +0200

    Added profile to test to fix logging issues

--- src/test/kotlin/no/nav/klage/oppgave/api/mapper/KlagebehandlingMapperTest.kt
+++ src/test/kotlin/no/nav/klage/oppgave/api/mapper/KlagebehandlingMapperTest.kt
@@ -15,9 +15,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDateTime
 import java.util.*
 
+@ActiveProfiles("local")
 @SpringBootTest(classes = [KlagebehandlingMapper::class])
 class KlagebehandlingMapperTest {
     @MockkBean
