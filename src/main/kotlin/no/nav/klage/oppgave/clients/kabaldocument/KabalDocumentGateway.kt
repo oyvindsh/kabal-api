6c3fa36b2d3a9672ed34c7525e26748a8fd674a5
Hågen Hasle <haagenhasle@gmail.com>
2021-10-21 14:26:48 +0200

    Fjernet død kode

--- src/main/kotlin/no/nav/klage/oppgave/clients/kabaldocument/KabalDocumentGateway.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/kabaldocument/KabalDocumentGateway.kt
@@ -1,7 +1,6 @@
 package no.nav.klage.oppgave.clients.kabaldocument
 
 import no.nav.klage.oppgave.clients.kabaldocument.model.request.FilInput
-import no.nav.klage.oppgave.clients.kabaldocument.model.response.DokumentEnhetOutput
 import no.nav.klage.oppgave.domain.DokumentInnhold
 import no.nav.klage.oppgave.domain.DokumentInnholdOgTittel
 import no.nav.klage.oppgave.domain.DokumentMetadata
@@ -34,12 +33,6 @@ class KabalDocumentGateway(
                 kabalDocumentMapper.mapKlagebehandlingToDokumentEnhetInput(klagebehandling)
             ).id
         )
-    }
-
-    fun getDokumentEnhet(
-        dokumentEnhetId: UUID,
-    ): DokumentEnhetOutput? {
-        return null
     }
 
     fun uploadHovedDokument(
