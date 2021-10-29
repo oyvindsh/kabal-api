01babdaf985c8ecfa701610283cf83b616e61fed
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-29 14:00:39 +0200

    Removing unused code (vedtaksbrev)

--- src/main/kotlin/no/nav/klage/oppgave/domain/vedtaksbrev/enums/BrevElementInputType.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.klage.oppgave.domain.vedtaksbrev.enums
-
-enum class BrevElementInputType {
-    FREE_TEXT_INPUT,
-    SINGLE_LINE_INPUT,
-    SINGLE_LINE_PREFILLED,
-    MULTIPLE_LINES_PREFILLED
-}
