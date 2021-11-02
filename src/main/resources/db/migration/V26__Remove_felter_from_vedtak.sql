8ebab20340a4a748e76c43954bfa71d1961cc4d0
Hågen Hasle <haagenhasle@gmail.com>
2021-11-02 20:36:58 +0100

    Lagt inn igjen flyway-migreringen

--- src/main/resources/db/migration/V26__Remove_felter_from_vedtak.sql
+++ src/main/resources/db/migration/V26__Remove_felter_from_vedtak.sql
@@ -1,6 +1,6 @@
---ALTER TABLE klage.vedtak
---    DROP COLUMN opplastet,
---    DROP COLUMN ferdig_distribuert,
---    DROP COLUMN mellomlager_id;
+ALTER TABLE klage.vedtak
+    DROP COLUMN opplastet,
+    DROP COLUMN ferdig_distribuert,
+    DROP COLUMN mellomlager_id;
 
---DROP TABLE klage.brevmottaker;
+DROP TABLE klage.brevmottaker;
