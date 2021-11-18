fef6ef94fccd30008e75a24c4ecf52cffa8da4eb
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-11-18 15:08:13 +0100

    Adjusted successful validation response.

--- src/main/kotlin/no/nav/klage/oppgave/api/view/ValidationPassedResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/ValidationPassedResponse.kt
@@ -1,12 +1,12 @@
 package no.nav.klage.oppgave.api.view
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import no.nav.klage.oppgave.exceptions.InvalidProperty
+import no.nav.klage.oppgave.exceptions.ValidationSection
 
 /** Only used when validation passed */
 data class ValidationPassedResponse(
     val title: String = "Validation success",
     val status: Int = 200,
-    @JsonProperty("invalid-properties")
-    val invalidProperties: List<InvalidProperty> = emptyList()
+    @JsonProperty("sections")
+    val sections: List<ValidationSection> = emptyList()
 )
