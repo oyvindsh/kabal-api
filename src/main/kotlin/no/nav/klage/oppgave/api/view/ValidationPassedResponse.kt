adbeca0313c1f22542ae7364fed1fd6a30fadc60
Andreas Jonsson <andreas.jonsson@nav.no>
2021-11-18 15:48:11 +0100

    Removed unnecessary annotation

--- src/main/kotlin/no/nav/klage/oppgave/api/view/ValidationPassedResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/ValidationPassedResponse.kt
@@ -1,12 +1,10 @@
 package no.nav.klage.oppgave.api.view
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.klage.oppgave.exceptions.ValidationSection
 
 /** Only used when validation passed */
 data class ValidationPassedResponse(
     val title: String = "Validation success",
     val status: Int = 200,
-    @JsonProperty("sections")
     val sections: List<ValidationSection> = emptyList()
 )
