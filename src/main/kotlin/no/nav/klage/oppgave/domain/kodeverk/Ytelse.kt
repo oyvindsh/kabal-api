6575a28e8e7f8141faf4a2697a9407507d4d6aed
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-11-24 10:19:19 +0100

    Added enhet identifiers from Axsys.

--- src/main/kotlin/no/nav/klage/oppgave/domain/kodeverk/Ytelse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/domain/kodeverk/Ytelse.kt
@@ -10,8 +10,13 @@ enum class Ytelse(override val id: String, override val navn: String, override v
     OMS_OLP("2", "Opplæringspenger", "Opplæringspenger"),
     OMS_PSB("3", "Pleiepenger sykt barn", "Pleiepenger sykt barn"),
     OMS_PLS("4", "Pleiepenger i livets sluttfase", "Pleiepenger i livets sluttfase"),
-    SYK_SYK("5", "Sykepenger", "Sykepenger")
+    SYK_SYK("5", "Sykepenger", "Sykepenger"),
+    //TODO: Koordiner disse med andre apper som bruker dette kodeverket.
+    FOR_FOR("6", "Foreldrepenger", "Foreldrepenger"),
+    FOR_ENG("7", "Engangsstønad", "Engangsstønad"),
+    FOR_SVA("8", "Svangerskapspenger", "Svangerskapspenger"),
     ;
+
 
     companion object {
         fun of(id: String): Ytelse {
@@ -24,26 +29,10 @@ enum class Ytelse(override val id: String, override val navn: String, override v
         return when(this) {
             OMS_OMP, OMS_OLP, OMS_PSB, OMS_PLS -> Tema.OMS
             SYK_SYK -> Tema.SYK
+            FOR_FOR, FOR_ENG, FOR_SVA -> Tema.FOR
         }
     }
 }
-
-val ytelserPerEnhet = mapOf(
-    "4203" to listOf(Ytelse.SYK_SYK),
-    "4205" to listOf(Ytelse.SYK_SYK),
-    "4207" to listOf(),
-    "4214" to listOf(Ytelse.SYK_SYK),
-    "4219" to listOf(Ytelse.OMS_OMP, Ytelse.OMS_PLS, Ytelse.OMS_PSB, Ytelse.OMS_OLP),
-    "4250" to listOf(),
-)
-
-val enheterPerYtelse = mapOf(
-    Ytelse.SYK_SYK to listOf("4203", "4205", "4214"),
-    Ytelse.OMS_OMP to listOf("4219"),
-    Ytelse.OMS_PLS to listOf("4219"),
-    Ytelse.OMS_PSB to listOf("4219"),
-    Ytelse.OMS_OLP to listOf("4219"),
-)
 
 object LovligeYtelser {
     private val lovligeYtelserIProdGcp = EnumSet.of(Ytelse.OMS_OMP, Ytelse.OMS_OLP, Ytelse.OMS_PSB, Ytelse.OMS_PLS)
@@ -65,3 +54,31 @@ class YtelseConverter : AttributeConverter<Ytelse, String?> {
     override fun convertToEntityAttribute(id: String?): Ytelse? =
         id?.let { Ytelse.of(it) }
 }
+
+
+val ytelserPerEnhet = mapOf(
+    "4203" to listOf(Ytelse.SYK_SYK),
+    "4205" to listOf(Ytelse.FOR_FOR, Ytelse.FOR_ENG, Ytelse.FOR_SVA, Ytelse.SYK_SYK),
+    "4207" to listOf(),
+    "4214" to listOf(Ytelse.SYK_SYK),
+    "4219" to listOf(Ytelse.OMS_OMP, Ytelse.OMS_PLS, Ytelse.OMS_PSB, Ytelse.OMS_OLP),
+    "4250" to listOf(),
+
+    "4293" to listOf(Ytelse.SYK_SYK),
+    "4292" to listOf(Ytelse.FOR_FOR, Ytelse.FOR_ENG, Ytelse.FOR_SVA, Ytelse.SYK_SYK),
+    "4293" to listOf(),
+    "4294" to listOf(Ytelse.SYK_SYK),
+    "4295" to listOf(Ytelse.OMS_OMP, Ytelse.OMS_PLS, Ytelse.OMS_PSB, Ytelse.OMS_OLP),
+    "4250" to listOf(),
+)
+
+val enheterPerYtelse = mapOf(
+    Ytelse.SYK_SYK to listOf("4203", "4293", "4205", "4292", "4214", "4294"),
+    Ytelse.FOR_SVA to listOf("4205", "4292"),
+    Ytelse.FOR_ENG to listOf("4205", "4292"),
+    Ytelse.FOR_FOR to listOf("4205", "4292"),
+    Ytelse.OMS_OMP to listOf("4219", "4295"),
+    Ytelse.OMS_PLS to listOf("4219", "4295"),
+    Ytelse.OMS_PSB to listOf("4219", "4295"),
+    Ytelse.OMS_OLP to listOf("4219", "4295"),
+)
