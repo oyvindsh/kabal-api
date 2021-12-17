e957d90abf2ae137f6c7c9b1557af7c7102d3fa3
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-17 13:26:41 +0100

    Removed unused fields

--- src/main/kotlin/no/nav/klage/oppgave/domain/klage/MottakHjemmel.kt
+++ src/main/kotlin/no/nav/klage/oppgave/domain/klage/MottakHjemmel.kt
@@ -1,21 +1,16 @@
 package no.nav.klage.oppgave.domain.klage
 
-import no.nav.klage.kodeverk.hjemmel.LovKilde
 import java.util.*
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
 @Table(name = "mottak_hjemmel", schema = "klage")
 class MottakHjemmel(
     @Id
     val id: UUID = UUID.randomUUID(),
-    @Column(name = "lov")
-    @Enumerated(EnumType.STRING)
-    val lov: LovKilde? = null,
-    @Column(name = "kapittel")
-    val kapittel: Int? = null,
-    @Column(name = "paragraf")
-    val paragraf: Int? = null,
     @Column(name = "hjemmel_id")
     val hjemmelId: String
 ) {
