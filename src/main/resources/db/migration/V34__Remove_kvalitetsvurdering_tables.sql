b70a7452c33c190f88f9ca12359566a5dfa9275f
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-01-06 15:44:46 +0100

    Changed db operation order.

--- src/main/resources/db/migration/V34__Remove_kvalitetsvurdering_tables.sql
+++ src/main/resources/db/migration/V34__Remove_kvalitetsvurdering_tables.sql
@@ -1,2 +1,2 @@
-DROP TABLE klage.kvalitetsvurdering_avvik_oversendelsesbrev, klage.kvalitetsvurdering_avvik_utredning, klage.kvalitetsvurdering_avvik_vedtak, klage.kvalitetsvurdering;
 DROP INDEX klage.klagebehandling_kvalitetsvurdering_idx, klage.kvalitetsvurdering_ao_kvalitetsvurdering_idx, klage.kvalitetsvurdering_au_kvalitetsvurdering_idx, klage.kvalitetsvurdering_av_kvalitetsvurdering_idx;
+DROP TABLE klage.kvalitetsvurdering_avvik_oversendelsesbrev, klage.kvalitetsvurdering_avvik_utredning, klage.kvalitetsvurdering_avvik_vedtak, klage.kvalitetsvurdering;
