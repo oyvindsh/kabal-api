5c2640c63c59a1fe83ae282a8a5a539061a483b5
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-01-18 14:16:22 +0100

    Adjusted flyway script.

--- src/main/resources/db/migration/V38__Ankebehandling.sql
+++ src/main/resources/db/migration/V38__Ankebehandling.sql
@@ -1,16 +1,16 @@
-CREATE TABLE klage.ankebehandling_registreringshjemmel
-(
-    id                TEXT,
-    ankebehandling_id UUID
-        CONSTRAINT fk_registreringshjemmel_ankebehandling
-            REFERENCES behandling (id),
-    PRIMARY KEY (id, ankebehandling_id)
-);
-
-CREATE INDEX IF NOT EXISTS ankebehandling_registreringshjemmel_ankebehandling_idx
-    ON ankebehandling_registreringshjemmel (ankebehandling_id);
-
 ALTER TABLE klage.behandling
     ADD COLUMN dato_vedtak_klageinstans    DATE,
     ADD COLUMN avsender_enhet_klageinstans TEXT,
-    ADD COLUMN forrige_klagebehandling_id UUID;
+    ADD COLUMN forrige_klagebehandling_id  UUID;
+
+CREATE TABLE klage.ankebehandling_registreringshjemmel
+(
+    id                TEXT,
+    ankebehandling_id UUID,
+    PRIMARY KEY (id, ankebehandling_id)
+);
+
+ALTER TABLE klage.ankebehandling_registreringshjemmel
+    ADD CONSTRAINT fk_registreringshjemmel_ankebehandling
+        FOREIGN KEY (ankebehandling_id)
+            REFERENCES klage.behandling (id);
