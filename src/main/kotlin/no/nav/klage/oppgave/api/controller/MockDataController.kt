5e3cc82e3b89cf1f96d272448549c728a7e83750
Andreas Jonsson <andreas.jonsson@nav.no>
2022-03-02 10:55:56 +0100

    Return id for behandling for e2e-tests

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/MockDataController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/MockDataController.kt
@@ -26,93 +26,6 @@ class MockDataController(
     // Alle syntetiske personer under her er registrert under https://dolly.dev.adeo.no/gruppe/2960
 
     @Unprotected
-    @PostMapping("/randomklage")
-    fun sendInnRandomKlage() {
-        val dollyDoc = listOf(
-            SyntheticWithDoc("02446701749", "510534792"),
-            SyntheticWithDoc("29437117843", "510534815"),
-            SyntheticWithDoc("25438301286", "510534816"),
-            SyntheticWithDoc("18496900509", "510534817"),
-            SyntheticWithDoc("28416904490", "510534818"),
-            SyntheticWithDoc("17457337760", "510534819"),
-            SyntheticWithDoc("16498818653", "510534820"),
-            SyntheticWithDoc("20467938577", "510534821"),
-            SyntheticWithDoc("14437830275", "510534823"),
-            SyntheticWithDoc("18418507701", "510534797"),
-            SyntheticWithDoc("12518603068", "510534824")
-        ).random()
-
-        val fnr = dollyDoc.fnr
-        val journalpostId = dollyDoc.journalpost
-        val journalpost = safClient.getJournalpostAsSystembruker(journalpostId)
-
-        val dato = LocalDate.of(2020, (1..12).random(), (1..28).random())
-
-        val randomYtelse = listOf(
-            Ytelse.SYK_SYK,
-            Ytelse.OMS_OLP,
-            Ytelse.OMS_OMP,
-            Ytelse.OMS_PLS,
-            Ytelse.OMS_PSB
-        ).random()
-
-        val randomHjemmelList = if (randomYtelse == Ytelse.SYK_SYK) {
-            listOf(
-                listOf(
-                    Hjemmel.FTRL_8_2,
-                    Hjemmel.FTRL_8_3,
-                    Hjemmel.FTRL_8_4,
-                    Hjemmel.FTRL_8_8,
-                    Hjemmel.FTRL_8_13,
-                )
-                    .random()
-            )
-        } else {
-            listOf(
-                listOf(
-                    Hjemmel.FTRL_8_13,
-                    Hjemmel.FTRL_9_2,
-                    Hjemmel.FTRL_9_3,
-                    Hjemmel.FTRL_9_11,
-                    Hjemmel.FTRL_9_14,
-                    Hjemmel.FTRL_22_13
-                )
-                    .random()
-            )
-        }
-
-        mottakService.createMottakForKlageV2(
-            OversendtKlageV2(
-                ytelse = randomYtelse,
-                type = Type.KLAGE,
-                klager = OversendtKlager(
-                    id = OversendtPartId(OversendtPartIdType.PERSON, fnr)
-                ),
-                fagsak = journalpost?.sak?.let {
-                    OversendtSak(
-                        fagsakId = it.fagsakId ?: "UKJENT",
-                        fagsystem = KildeFagsystem.AO01
-                    )
-                },
-                kildeReferanse = UUID.randomUUID().toString(),
-                innsynUrl = "https://nav.no",
-                hjemler = randomHjemmelList,
-                avsenderSaksbehandlerIdent = "Z994674",
-                avsenderEnhet = "0104", //NAV Moss
-                tilknyttedeJournalposter = listOf(
-                    OversendtDokumentReferanse(
-                        randomMottakDokumentType(),
-                        journalpostId
-                    )
-                ),
-                mottattFoersteinstans = dato,
-                innsendtTilNav = dato.minusDays(3),
-                kilde = KildeFagsystem.AO01
-            )
-        )
-    }
-
-    @Unprotected
     @PostMapping("/kode6")
     fun createKode6Person() {
         val fnr = "15436621822" // ÅPENHJERTIG SAKS
@@ -250,7 +163,7 @@ class MockDataController(
 
     @Unprotected
     @PostMapping("/randomanke")
-    fun sendInnRandomAnke() {
+    fun sendInnRandomAnke(): UUID {
         val dollyDoc = listOf(
             SyntheticWithDoc("02446701749", "510534792"),
             SyntheticWithDoc("29437117843", "510534815"),
@@ -304,7 +217,7 @@ class MockDataController(
             )
         }
 
-        mottakService.createMottakForKlageAnkeV3(
+        return mottakService.createMottakForKlageAnkeV3ForE2ETests(
             OversendtKlageAnkeV3(
                 ytelse = randomYtelse,
                 type = Type.ANKE,
@@ -332,7 +245,94 @@ class MockDataController(
                 innsendtTilNav = dato.minusDays(3),
                 kilde = KildeFagsystem.AO01,
             )
-        )
+        ).id
+    }
+
+    @Unprotected
+    @PostMapping("/randomklage")
+    fun sendInnRandomKlage(): UUID {
+        val dollyDoc = listOf(
+            SyntheticWithDoc("02446701749", "510534792"),
+            SyntheticWithDoc("29437117843", "510534815"),
+            SyntheticWithDoc("25438301286", "510534816"),
+            SyntheticWithDoc("18496900509", "510534817"),
+            SyntheticWithDoc("28416904490", "510534818"),
+            SyntheticWithDoc("17457337760", "510534819"),
+            SyntheticWithDoc("16498818653", "510534820"),
+            SyntheticWithDoc("20467938577", "510534821"),
+            SyntheticWithDoc("14437830275", "510534823"),
+            SyntheticWithDoc("18418507701", "510534797"),
+            SyntheticWithDoc("12518603068", "510534824")
+        ).random()
+
+        val fnr = dollyDoc.fnr
+        val journalpostId = dollyDoc.journalpost
+        val journalpost = safClient.getJournalpostAsSystembruker(journalpostId)
+
+        val dato = LocalDate.of(2020, (1..12).random(), (1..28).random())
+
+        val randomYtelse = listOf(
+            Ytelse.SYK_SYK,
+            Ytelse.OMS_OLP,
+            Ytelse.OMS_OMP,
+            Ytelse.OMS_PLS,
+            Ytelse.OMS_PSB
+        ).random()
+
+        val randomHjemmelList = if (randomYtelse == Ytelse.SYK_SYK) {
+            listOf(
+                listOf(
+                    Hjemmel.FTRL_8_2,
+                    Hjemmel.FTRL_8_3,
+                    Hjemmel.FTRL_8_4,
+                    Hjemmel.FTRL_8_8,
+                    Hjemmel.FTRL_8_13,
+                )
+                    .random()
+            )
+        } else {
+            listOf(
+                listOf(
+                    Hjemmel.FTRL_8_13,
+                    Hjemmel.FTRL_9_2,
+                    Hjemmel.FTRL_9_3,
+                    Hjemmel.FTRL_9_11,
+                    Hjemmel.FTRL_9_14,
+                    Hjemmel.FTRL_22_13
+                )
+                    .random()
+            )
+        }
+
+        return mottakService.createMottakForKlageAnkeV3ForE2ETests(
+            OversendtKlageAnkeV3(
+                ytelse = randomYtelse,
+                type = Type.ANKE,
+                klager = OversendtKlager(
+                    id = OversendtPartId(OversendtPartIdType.PERSON, fnr)
+                ),
+                fagsak = journalpost?.sak?.let {
+                    OversendtSak(
+                        fagsakId = it.fagsakId ?: "UKJENT",
+                        fagsystem = KildeFagsystem.AO01
+                    )
+                },
+                kildeReferanse = UUID.randomUUID().toString(),
+                innsynUrl = "https://nav.no",
+                hjemler = randomHjemmelList,
+                forrigeBehandlendeEnhet = "4295", //NAV Klageinstans nord
+                tilknyttedeJournalposter = listOf(
+                    OversendtDokumentReferanse(
+                        randomMottakDokumentType(),
+                        journalpostId
+                    )
+                ),
+                brukersHenvendelseMottattNavDato = dato,
+                sakMottattKaDato = dato.atStartOfDay(),
+                innsendtTilNav = dato.minusDays(3),
+                kilde = KildeFagsystem.AO01,
+            )
+        ).id
     }
 
     private fun randomMottakDokumentType() = listOf(
