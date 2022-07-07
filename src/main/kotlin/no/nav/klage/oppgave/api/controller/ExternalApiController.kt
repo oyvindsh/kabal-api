ff7e38b8c98be1ad58705b1e5d3b78fda2e53a08
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-07-07 15:22:05 +0200

    Added proper rest point.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/ExternalApiController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/ExternalApiController.kt
@@ -3,13 +3,19 @@ package no.nav.klage.oppgave.api.controller
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
+import no.nav.klage.oppgave.api.view.OversendtAnkeITrygderettenV1
 import no.nav.klage.oppgave.api.view.OversendtKlageAnkeV3
 import no.nav.klage.oppgave.api.view.OversendtKlageV2
 import no.nav.klage.oppgave.config.SecurityConfiguration
+import no.nav.klage.oppgave.service.AnkeITrygderettenbehandlingService
 import no.nav.klage.oppgave.service.MottakService
 import no.nav.klage.oppgave.util.getLogger
+import no.nav.klage.oppgave.util.getSecureLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
@@ -17,12 +23,14 @@ import javax.validation.Valid
 @ProtectedWithClaims(issuer = SecurityConfiguration.ISSUER_AAD)
 @RequestMapping("api")
 class ExternalApiController(
-    private val mottakService: MottakService
+    private val mottakService: MottakService,
+    private val ankeITrygderettenbehandlingService: AnkeITrygderettenbehandlingService,
 ) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
     }
 
     @ApiOperation(
@@ -50,14 +58,14 @@ class ExternalApiController(
     }
 
     @ApiOperation(
-        value = "Hent informasjon om en klagebehandling. Ikke implementert.",
-        notes = "Endepunkt for å se detaljert informasjon om en klagebehandling. Ikke implementert."
+        value = "Send inn anker i trygderetten til Kabal",
+        notes = "Endepunkt for å registrere anker som allerede har blitt oversendt til Trygderetten i Kabal"
     )
-    @GetMapping("innsyn/v1/behandling/{id}")
-    fun innsynBehandling(
-        @ApiParam(value = "Id for klagebehandling")
-        @PathVariable("id") behandlingId: String
-    ): String {
-        return "Not implemented yet"
+    @PostMapping("/ankeritrygderetten")
+    fun sendInnAnkeITrygderettenV1(
+        @Valid @RequestBody oversendtAnkeITrygderetten: OversendtAnkeITrygderettenV1
+    ) {
+        secureLogger.debug("Ankeitrygderetten data $oversendtAnkeITrygderetten sent to Kabal")
+        ankeITrygderettenbehandlingService.createAnkeITrygderettenbehandling(oversendtAnkeITrygderetten)
     }
 }
