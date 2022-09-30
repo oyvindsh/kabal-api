baaefd47e123f92fd32baa1d8e1c5251c0823337
Andreas Jonsson <andreas.jonsson@nav.no>
2022-09-30 15:21:56 +0200

    Corrected migration query
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/resources/db/migration/V56__Migrate_utfall.sql
+++ src/main/resources/db/migration/V56__Migrate_utfall.sql
@@ -1,15 +1,13 @@
-UPDATE d
-SET d.utfall_id = '9'
-FROM klage.delbehandling d,
-     klage.behandling b
+UPDATE klage.delbehandling d
+SET utfall_id = '9'
+FROM klage.behandling b
 WHERE d.utfall_id = '6'
   AND b.id = d.behandling_id
   AND b.behandling_type = 'anke';
 
-UPDATE d
-SET d.utfall_id = '10'
-FROM klage.delbehandling d,
-     klage.behandling b
+UPDATE klage.delbehandling d
+SET utfall_id = '10'
+FROM klage.behandling b
 WHERE d.utfall_id = '8'
   AND b.id = d.behandling_id
   AND b.behandling_type = 'anke';
