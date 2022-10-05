e56b67e46e4dcb1a96d817e84bcfd51a076588e9
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-10-05 15:48:51 +0200

    Remove unused property.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/clients/kabaldocument/model/response/BrevmottakerOutput.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/kabaldocument/model/response/BrevmottakerOutput.kt
@@ -1,8 +1,9 @@
 package no.nav.klage.oppgave.clients.kabaldocument.model.response
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 class BrevmottakerOutput(
     val partId: PartIdOutput,
     val navn: String,
-    val rolle: String
 )
-
