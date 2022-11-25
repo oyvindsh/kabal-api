7f6f6f8abff4a2c66cd95f1ccbddd4f580e190c2
Andreas Jonsson <andreas.jonsson@nav.no>
2022-11-25 11:38:00 +0100

    New response for tildeling.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/api/controller/BehandlingAssignmentController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/controller/BehandlingAssignmentController.kt
@@ -2,9 +2,11 @@ package no.nav.klage.oppgave.api.controller
 
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
-import no.nav.klage.oppgave.api.view.Saksbehandlertildeling
-import no.nav.klage.oppgave.api.view.TildelingEditedView
+import no.nav.klage.oppgave.api.view.*
+import no.nav.klage.oppgave.clients.egenansatt.EgenAnsattService
+import no.nav.klage.oppgave.clients.pdl.PdlFacade
 import no.nav.klage.oppgave.config.SecurityConfiguration.Companion.ISSUER_AAD
+import no.nav.klage.oppgave.domain.Behandling
 import no.nav.klage.oppgave.service.BehandlingService
 import no.nav.klage.oppgave.service.InnloggetSaksbehandlerService
 import no.nav.klage.oppgave.service.SaksbehandlerService
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.chrono.ChronoLocalDateTime
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 @RestController
@@ -22,7 +28,9 @@ import java.util.*
 class BehandlingAssignmentController(
     private val innloggetSaksbehandlerService: InnloggetSaksbehandlerService,
     private val saksbehandlerService: SaksbehandlerService,
-    private val behandlingService: BehandlingService
+    private val behandlingService: BehandlingService,
+    private val pdlFacade: PdlFacade,
+    private val egenAnsattService: EgenAnsattService,
 ) {
 
     companion object {
@@ -46,10 +54,7 @@ class BehandlingAssignmentController(
             enhetId = saksbehandlerService.getEnhetForSaksbehandler(saksbehandlertildeling.navIdent).enhetId,
             utfoerendeSaksbehandlerIdent = innloggetSaksbehandlerService.getInnloggetIdent()
         )
-        return TildelingEditedView(
-            behandling.modified,
-            behandling.tildeling!!.tidspunkt.toLocalDate()
-        )
+        return tildelingEditedView(behandling)
     }
 
     @PostMapping("/behandlinger/{id}/saksbehandlertildeling")
@@ -65,10 +70,7 @@ class BehandlingAssignmentController(
             enhetId = saksbehandlerService.getEnhetForSaksbehandler(saksbehandlertildeling.navIdent).enhetId,
             utfoerendeSaksbehandlerIdent = innloggetSaksbehandlerService.getInnloggetIdent()
         )
-        return TildelingEditedView(
-            behandling.modified,
-            behandling.tildeling!!.tidspunkt.toLocalDate()
-        )
+        return tildelingEditedView(behandling)
     }
 
     //TODO remove when FE migrated to new endpoint without navident
@@ -86,11 +88,7 @@ class BehandlingAssignmentController(
             null,
             innloggetSaksbehandlerService.getInnloggetIdent()
         )
-
-        return TildelingEditedView(
-            behandling.modified,
-            behandling.tildeling!!.tidspunkt.toLocalDate()
-        )
+        return tildelingEditedView(behandling)
     }
 
     @PostMapping("/behandlinger/{id}/saksbehandlerfradeling")
@@ -105,11 +103,63 @@ class BehandlingAssignmentController(
             null,
             innloggetSaksbehandlerService.getInnloggetIdent()
         )
+        return tildelingEditedView(behandling)
+    }
 
-        return TildelingEditedView(
-            behandling.modified,
-            behandling.tildeling!!.tidspunkt.toLocalDate()
+    private fun tildelingEditedView(behandling: Behandling): TildelingEditedView {
+        val fnr = behandling.sakenGjelder.partId.value
+        val personInfo = pdlFacade.getPersonInfo(fnr)
+
+        val erFortrolig = personInfo.harBeskyttelsesbehovFortrolig()
+        val erStrengtFortrolig = personInfo.harBeskyttelsesbehovStrengtFortrolig()
+        val erEgenAnsatt = egenAnsattService.erEgenAnsatt(fnr)
+
+        val tildelingEditedView = TildelingEditedView(
+            id = behandling.id.toString(),
+            person = PersonView(
+                fnr = fnr,
+                navn = personInfo.sammensattNavn,
+                sivilstand = null
+            ),
+            type = behandling.type.id,
+            ytelse = behandling.ytelse.id,
+            tema = behandling.ytelse.toTema().id,
+            hjemmel = behandling.hjemler.firstOrNull()?.id,
+            frist = behandling.frist,
+            mottatt = behandling.mottattKlageinstans.toLocalDate(),
+            erMedunderskriver = behandling.medunderskriver?.saksbehandlerident == innloggetSaksbehandlerService.getInnloggetIdent(),
+            harMedunderskriver = behandling.medunderskriver != null,
+            medunderskriverident = behandling.medunderskriver?.saksbehandlerident,
+            medunderskriverFlyt = behandling.medunderskriverFlyt,
+            utfall = null,
+            avsluttetAvSaksbehandlerDate = null,
+            isAvsluttetAvSaksbehandler = false,
+            erTildelt = behandling.tildeling != null,
+            tildeltSaksbehandlerident = behandling.tildeling?.saksbehandlerident,
+            tildeltSaksbehandlerNavn = if (behandling.tildeling != null) {
+                saksbehandlerService.getNameForIdent(behandling.tildeling!!.saksbehandlerident!!)
+            } else null,
+            saksbehandlerHarTilgang = true,
+            egenAnsatt = erEgenAnsatt,
+            fortrolig = erFortrolig,
+            strengtFortrolig = erStrengtFortrolig,
+            ageKA = behandling.mottattKlageinstans.toAgeInDays(),
+            access = AccessView.ASSIGN,
+            sattPaaVent = behandling.toSattPaaVent(),
         )
+        return tildelingEditedView
+    }
+
+    private fun LocalDateTime.toAgeInDays() = ChronoUnit.DAYS.between(this.toLocalDate(), LocalDate.now()).toInt()
+
+    private fun Behandling.toSattPaaVent(): Venteperiode? {
+        return if (sattPaaVent != null) {
+            val expires = sattPaaVent!!.plusWeeks(4)
+            Venteperiode(
+                from = sattPaaVent!!.toLocalDate(),
+                to = expires.toLocalDate(),
+                isExpired = expires.isBefore(ChronoLocalDateTime.from(LocalDateTime.now()))
+            )
+        } else null
     }
 }
-
--- src/main/kotlin/no/nav/klage/oppgave/api/view/TildelingEditedView.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/TildelingEditedView.kt
@@ -1,6 +1,48 @@
 package no.nav.klage.oppgave.api.view
 
+import no.nav.klage.kodeverk.MedunderskriverFlyt
 import java.time.LocalDate
-import java.time.LocalDateTime
 
-class TildelingEditedView(val modified: LocalDateTime, val tildelt: LocalDate)
+data class TildelingEditedView(
+    val id: String,
+    val person: PersonView? = null,
+    val type: String,
+    val ytelse: String?,
+    val tema: String,
+    val hjemmel: String?,
+    val frist: LocalDate?,
+    val mottatt: LocalDate?,
+    val erMedunderskriver: Boolean = false,
+    val harMedunderskriver: Boolean = false,
+    val medunderskriverident: String?,
+    val medunderskriverFlyt: MedunderskriverFlyt,
+    val utfall: String?,
+    val avsluttetAvSaksbehandlerDate: LocalDate?,
+    val isAvsluttetAvSaksbehandler: Boolean,
+    val erTildelt: Boolean,
+    val tildeltSaksbehandlerident: String?,
+    val tildeltSaksbehandlerNavn: String?,
+    val saksbehandlerHarTilgang: Boolean,
+    val egenAnsatt: Boolean,
+    val fortrolig: Boolean,
+    val strengtFortrolig: Boolean,
+    val ageKA: Int,
+    val access: AccessView,
+    val sattPaaVent: Venteperiode?,
+)
+
+data class PersonView(
+    val fnr: String?,
+    val navn: String?,
+    val sivilstand: String? = null
+)
+
+enum class AccessView {
+    NONE, READ, ASSIGN, WRITE
+}
+
+data class Venteperiode(
+    val from: LocalDate?,
+    val to: LocalDate?,
+    val isExpired: Boolean?,
+)
