b1931a1074ea6714a15368667aa782c2984b38b3
Andreas Jonsson <andreas.jonsson@nav.no>
2022-12-07 14:58:19 +0100

    Ignoring unknown properties from Kaka. Preparing for some changes.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/kotlin/no/nav/klage/oppgave/clients/kaka/model/response/KakaOutput.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/kaka/model/response/KakaOutput.kt
@@ -1,9 +1,11 @@
 package no.nav.klage.oppgave.clients.kaka.model.response
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.util.*
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class KakaOutput(
-    val id: UUID
+    val id: UUID,
 )
 
 data class ValidationErrors(
