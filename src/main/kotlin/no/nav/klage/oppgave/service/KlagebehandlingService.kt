5569c2863323265d11374a30e08da5f7b65fa28a
Andreas Jonsson <andreas.jonsson@nav.no>
2023-02-16 21:49:17 +0100

    Experimenting with no transaction

--- src/main/kotlin/no/nav/klage/oppgave/service/KlagebehandlingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/KlagebehandlingService.kt
@@ -16,6 +16,7 @@ import no.nav.klage.oppgave.util.getSecureLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 import java.util.*
@@ -47,6 +48,7 @@ class KlagebehandlingService(
         Utfall.AVVIST
     )
 
+    @Transactional(propagation = Propagation.NEVER)
     fun findCompletedKlagebehandlingerByPartIdValue(
         partIdValue: String
     ): List<CompletedKlagebehandling> {
