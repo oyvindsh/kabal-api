8d62b3fb60d8e3b59f5115d59e8481ee785c18e9
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-04-26 13:31:58 +0200

    Added endpoint for checking duplicate. Refactored Kabin integration classes.

--- src/main/kotlin/no/nav/klage/oppgave/api/view/kabin/KabinResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/kabin/KabinResponse.kt
@@ -1,9 +1,37 @@
-package no.nav.klage.oppgave.api.view
+package no.nav.klage.oppgave.api.view.kabin
 
 import no.nav.klage.kodeverk.Fagsystem
+import no.nav.klage.oppgave.api.view.BehandlingDetaljerView
+import no.nav.klage.oppgave.api.view.DokumentReferanse
+import no.nav.klage.oppgave.api.view.TilknyttetDokument
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
+
+data class CompletedKlagebehandling(
+    val behandlingId: UUID,
+    val ytelseId: String,
+    val utfallId: String,
+    val vedtakDate: LocalDateTime,
+    val sakenGjelder: BehandlingDetaljerView.SakenGjelderView,
+    val klager: BehandlingDetaljerView.KlagerView,
+    val fullmektig: BehandlingDetaljerView.ProsessfullmektigView?,
+    val tilknyttedeDokumenter: List<TilknyttetDokument>,
+    val sakFagsakId: String,
+    val fagsakId: String,
+    val sakFagsystem: Fagsystem,
+    val fagsystem: Fagsystem,
+    val fagsystemId: String,
+    val klageBehandlendeEnhet: String,
+)
+
+data class CreatedAnkeResponse(
+    val mottakId: UUID,
+)
+
+data class CreatedKlageResponse(
+    val mottakId: UUID,
+)
 
 data class CreatedAnkebehandlingStatusForKabin(
     val typeId: String,
