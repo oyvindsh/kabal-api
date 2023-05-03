f1a77519378a9f276b0eec55947d3920f6d1709e
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-05-03 13:36:33 +0200

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val jacksonJsonschemaVersion = "1.0.39"
 val pdfboxVersion = "2.0.28"
 val tikaVersion = "2.7.0"
 val verapdfVersion = "1.22.2"
-val klageKodeverkVersion = "1.3.0"
+val klageKodeverkVersion = "1.3.1"
 
 plugins {
     val kotlinVersion = "1.8.21"
