7240c176ca1b8cf7cb438d17b032d166f87049f2
Andreas Jonsson <andreas.jonsson@nav.no>
2023-05-16 11:17:56 +0200

    Mer logging ved tester
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,8 @@ tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
+        showStandardStreams = true
+        displayGranularity = -1
     }
 }
 
