f03addff04609baa2d42e80417d5d4b6d6eda37b
Andreas Jonsson <andreas.jonsson@nav.no>
2023-06-30 11:08:09 +0200

    Patch behandling with correct HJE-ytelse.
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

---
+++ src/main/resources/db/migration/V74__change_hje_ytelse.sql
@@ -0,0 +1,567 @@
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '272820';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '272820';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '272872';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '272872';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '273166';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '273166';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '273205';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '273205';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '273248';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '273248';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '273526';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '273526';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '273210';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '273210';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '273136';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '273136';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274494';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274494';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274199';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274199';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274724';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274724';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274718';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274718';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274700';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274700';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274698';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274698';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274846';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274846';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274955';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274955';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274991';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274991';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '274964';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '274964';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275292';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275292';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275205';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275205';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275300';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275300';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275267';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275267';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275503';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275503';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275521';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275521';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275595';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275595';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275549';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275549';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275565';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275565';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275594';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275594';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275601';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275601';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275713';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275713';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275748';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275748';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275746';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275746';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '275850';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '275850';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276025';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276025';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276031';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276031';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276315';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276315';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276332';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276332';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276407';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276407';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276752';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276752';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276808';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276808';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276773';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276773';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276812';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276812';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276861';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276861';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276876';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276876';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276970';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276970';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '276972';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '276972';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '276992';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '276992';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '276983';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '276983';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '276942';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '276942';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '276922';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '276922';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '274164';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '274164';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '273581';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '273581';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277054';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277054';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277072';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277072';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277104';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277104';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277129';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277129';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '277196';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '277196';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277199';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277199';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277260';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277260';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277274';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277274';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277280';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277280';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277303';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277303';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '277276';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '277276';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277299';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277299';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277185';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277185';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277305';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277305';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277302';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277302';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277292';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277292';
+
+update klage.behandling
+set ytelse_id = '50'
+where kilde_referanse = '277313';
+
+update klage.mottak
+set ytelse_id = '50'
+where kilde_referanse = '277313';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277341';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277341';
+
+update klage.behandling
+set ytelse_id = '49'
+where kilde_referanse = '277095';
+
+update klage.mottak
+set ytelse_id = '49'
+where kilde_referanse = '277095';
